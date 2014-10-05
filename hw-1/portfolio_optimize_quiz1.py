4e7128f39a679f1519c34eb0211614a02ebfee58
Hector Urroz <hurroz@tendrilinc.com>
2014-10-05 17:29:08 -0600

    Remove un-needed comments

--- hw-1/portfolio_optimize_quiz1.py
+++ hw-1/portfolio_optimize_quiz1.py
@@ -35,15 +35,6 @@ print "Volatility (std_dev of daily returns): ", best["std_deviation"]
 print "Average daily return: ", best["ave_daily_ret"]
 print "Cumulative return: ", best["cum_ret"]
 
-# Expected results:
-# Start Date: January 1, 2011
-# End Date: December 31, 2011
-# Symbols: ['AAPL', 'GLD', 'GOOG', 'XOM']
-# Optimal Allocations: [0.4, 0.4, 0.0, 0.2]
-# Sharpe Ratio: 1.02828403099
-# Volatility (stdev of daily returns):  0.0101467067654
-# Average Daily Return:  0.000657261102001
-# Cumulative Return:  1.16487261965
 
 
 
